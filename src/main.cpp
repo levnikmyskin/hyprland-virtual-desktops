e3bad3b9ab3dac25ad77641a2037601035125535
Alessio Molinari <alessio.molinari@strg.at>
2024-05-05 10:16:10 +0200

    chore: bump plugin version

--- src/main.cpp
+++ src/main.cpp
@@ -352,5 +352,5 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 
     // Initialize first vdesk
     HyprlandAPI::reloadConfig();
-    return {"virtual-desktops", "Virtual desktop like workspaces", "LevMyskin", "2.2.1"};
+    return {"virtual-desktops", "Virtual desktop like workspaces", "LevMyskin", "2.2.2"};
 }
